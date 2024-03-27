db2c88bc207af2ee5c6f6005ee29079f44a4d2d3
root <walkerkarl361@gmail.com>
2024-03-27 11:40:19 -0700

    done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 cos = number % 10 if number >= 10 else number % -10
-    print("Last digit of {:d} is {:d} and is ".format(number, cos), end = " ")
+    print("Last digit of {:d} is {:d} and is ".format(number, cos), end ="")
 if cos > 5:
     print("greater than 5")
 elif cos  == 0:
