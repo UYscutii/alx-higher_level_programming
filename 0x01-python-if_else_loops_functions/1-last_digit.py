686b22e42bb4f5fbfea207e0b9549c7e613f5a8d
root <walkerkarl361@gmail.com>
2024-03-27 11:29:03 -0700

    done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ cos = 0
 if number >= 0:
     cos = number % 10
 else:
-    cos = number %-10
+    cos = number % -10
     print("Last digit of {:d} is {:d} and is ".format(number, cos), end = " ")
 if cos > 5:
     print("greater than 5")
